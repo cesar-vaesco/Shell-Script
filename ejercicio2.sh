55a5ad262259eaf06f7e2f0621da634436490b46
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-10-07 13:43:57 -0500

    Primeros ejercicios con shellscript

---
+++ ejercicio2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# != 3 ]
+then
+	echo "Numero de parametros incorrectos"
+else
+	echo "Numero de prametros correctos"
+fi
---
+++ ejercicio3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ $# == 3 ]
+then
+	echo "$1 $2 $3"
+else
+	echo "Número de parametros incorrectos"
+fi
